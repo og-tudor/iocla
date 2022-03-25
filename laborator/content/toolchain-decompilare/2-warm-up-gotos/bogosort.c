cdfd9fa39a58c079477b97e896159cb2400d1168
og-tudor <tudor.frecus@gmail.com>
2022-03-25 13:21:07 +0200

    Solutia pentru lab04 ex2

--- laborator/content/toolchain-decompilare/2-warm-up-gotos/bogosort.c
+++ laborator/content/toolchain-decompilare/2-warm-up-gotos/bogosort.c
@@ -36,11 +36,12 @@ int main(void)
 		shuffle(numbers, 6);
 
 		if (is_sorted(numbers, 6))
-
+		goto skip;
 		/* TODO use goto instead of break */
-		break;
+		// break;
 	}
 
+skip:
 	for (i = 0; i < 6; i++)
 		printf("%d ", numbers[i]);
 	printf("\n");
